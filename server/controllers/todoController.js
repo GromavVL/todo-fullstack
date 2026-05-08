7c2d5d439a021de261a2412c411efee58bac7e79
Vlad <vlad086367@gmail.com>
2026-05-08 14:23:45 +0300

    Add endpoint for todo

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,7 +1,54 @@
-module.exports.getAllTodo = async (req, res, next) => {};
+const { Todo } = require('./../models');
 
-module.exports.createTodo = async (req, res, next) => {};
+module.exports.getAllTodo = async (req, res, next) => {
+  try {
+    const foundTodo = await Todo.findAll();
 
-module.exports.updateTodoById = async (req, res, next) => {};
+    res.status(200).send(foundTodo);
+  } catch (err) {
+    next(err);
+  }
+};
 
-module.exports.deleteTodoById = async (req, res, next) => {};
+module.exports.createTodo = async (req, res, next) => {
+  const { body } = req;
+  console.log('body :>> ', body);
+  try {
+    const createdTodo = await Todo.create(body);
+    res.status(201).send(createdTodo);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updateTodoById = async (req, res, next) => {
+  const {
+    params: { id },
+    body,
+  } = req;
+  console.log('params :>> ', id);
+  try {
+    const [countUpdateTodo, updatedTodo] = await Todo.update(body, {
+      where: { id },
+      returning: true,
+    });
+
+    res.status(200).send(updatedTodo);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteTodoById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deleteTodo = await Todo.destroy({ where: { id } });
+
+    if (!deleteTodo) {
+      return res.status(404).send('Todo not found');
+    }
+    res.status(200).send('Todo is deleted');
+  } catch (err) {
+    next(err);
+  }
+};
